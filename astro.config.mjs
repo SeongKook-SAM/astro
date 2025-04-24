b9c0f2bf5fcac9f922dcd05c365223e24745afb6
SeongKook-SAM <ksk8646@hotseller.co.kr>
2025-04-24 09:41:45 +0900

    Feature: sitemap, robots.txt 설치 및 적용

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://helpsns.co.kr',
+  integrations: [sitemap(), robotsTxt()],
 
   prefetch: {
     prefetchAll: true,
